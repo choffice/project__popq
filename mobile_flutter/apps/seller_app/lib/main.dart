ccaa25224a8a5a083af237a341e2fbd565aa8614
kominjung04 <heppy8888@naver.com>
2026-08-04 12:44:58 +0900

    feat:구글,카카오 로그인

--- mobile_flutter/apps/seller_app/lib/main.dart
+++ mobile_flutter/apps/seller_app/lib/main.dart
@@ -1,9 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/widgets.dart';
 import 'package:popq_app_core/popq_app_core.dart';
-
+import 'package:kakao_flutter_sdk_user/kakao_flutter_sdk_user.dart';
 import 'src/seller_app.dart';
 
-void main() {
+Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(PopqSellerApp(environment: AppEnvironment.fromEnvironment()));
+
+  await Firebase.initializeApp();
+  await KakaoSdk.init(
+    nativeAppKey: '7711e4885b55fb01d710c364b08c069e',
+  );
+
+  runApp(
+    PopqSellerApp(
+      environment: AppEnvironment.fromEnvironment(),
+    ),
+  );
 }
