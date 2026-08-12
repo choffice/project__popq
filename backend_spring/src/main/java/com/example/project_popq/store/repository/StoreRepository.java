94dd44e2b030b9fb5fc12b439e7b555a2e68ee58
choffice <choffice@naver.com>
2026-08-12 14:09:42 +0900

    conflict debugging(1)
    error msg

--- backend_spring/src/main/java/com/example/project_popq/store/repository/StoreRepository.java
+++ backend_spring/src/main/java/com/example/project_popq/store/repository/StoreRepository.java
@@ -8,6 +8,7 @@ import java.util.List;
 import java.util.Optional;
 import jakarta.persistence.LockModeType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
